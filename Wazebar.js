6c52693fc7e70bac5691df94dee37f979f971459
mythdraug-waze <46400507+mythdraug-waze@users.noreply.github.com>
2020-03-28 04:48:58 -0500

    Version bump

--- Wazebar.js
+++ Wazebar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Wazebar
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.03.20.01
+// @version      2020.03.28.01
 // @description  Displays a bar at the top of the editor that displays inbox, forum & wiki links
 // @author       JustinS83
 // @include      https://beta.waze.com/*
